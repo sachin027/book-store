b5194c6af442ece9f919ad4aed449e065f4ae717
Sachin Kanzariya <114856060+sachin027@users.noreply.github.com>
2023-03-16 09:05:28 +0530

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    render() {
+        return (
+          <div>
+            <p>This is About Page</p>
+          </div>
+        )
+    }
+}
+
+export default About
+
+
+
